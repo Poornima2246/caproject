726ad36e04a477f0576da8bc3e012030397c8dce
Poornima2246 <poornima220500@gmail.com>
2025-01-28 18:58:30 +0530

    Update Addpage.jsx

--- foodcort/src/Components/Addpage.jsx
+++ foodcort/src/Components/Addpage.jsx
@@ -182,7 +182,7 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function Addpage() {
-  const url = "https://caproject-server.onrender.com/";
+  const url = "https://caproject-server.onrender.com";
   const [formData, setFormData] = useState({
     mainImage: null,
     addImage1: null,
