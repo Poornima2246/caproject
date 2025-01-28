2c30f76cbf67213e23339c81696a9d5089e4099b
poornima2246 <poornima220500@gmail.com>
2025-01-28 15:35:24 +0530

    commit

--- server/Models/CakeModel.js
+++ server/Models/CakeModel.js
@@ -5,9 +5,9 @@ const CakeSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    mainImage: { type:  File, required: true }, // Main image filename
-    addImage1:{type: File, required: true},
-    addImage2:{type: File, required: true},
+    mainImage: { type:  String, required: true }, // Main image filename
+    addImage1:{type: String, required: true},
+    addImage2:{type: String, required: true},
     category: { type: String, required: true },
 
       // additionalImages: [{ type: String }], // Array of additional image filenames
